4a6c5360d54aa56f855a48e5543a72fee7462668
rob bygrave <robin.bygrave@gmail.com>
2017-09-29 23:55:51 +1300

    #29 - Change to use docker-run.properties to run tests against docker container for elasticsearch

--- src/test/java/integration/support/EmbeddedServer.java
+++ src/test/java/integration/support/EmbeddedServer.java
@@ -4,7 +4,7 @@ import io.ebean.DocumentStore;
 import io.ebean.Ebean;
 import io.ebean.EbeanServer;
 //import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.node.Node;
+//import org.elasticsearch.node.Node;
 //import org.elasticsearch.node.NodeBuilder;
 import org.example.domain.Contact;
 import org.example.domain.Country;
@@ -17,11 +17,7 @@ import org.example.domain.Vehicle;
  */
 public class EmbeddedServer {
 
-  final boolean useExternalElastic = true;
-
   final boolean indexOnStart = true;
-
-  final Node node;
 
   final EbeanServer server;
 
@@ -29,28 +25,6 @@ public class EmbeddedServer {
 
   public EmbeddedServer() {
 
-    if (useExternalElastic) {
-      this.node = null;
-
-    } else {
-
-//      Boot boot = new Boot();
-//      boot.start();
-//      Settings settings1 = Settings.builder()
-//          .put("path.home", "target/elastic-data")
-////          .put("number_of_shards", "1")
-////          .put("number_of_replicas", "1")
-////          .put("http.port", "9290")
-//
-//          .put("cluster.name", "EmbeddedTest")
-//          .put("http.type", "http.type.default")
-//          .put("transport.type", "local")
-//          .put("node.name", "foo")
-//          .build();
-//
-//      this.node = new Node(settings1);
-      this.node = null;
-    }
     server = Ebean.getDefaultServer();
     documentStore = server.docStore();
 
@@ -75,11 +49,6 @@ public class EmbeddedServer {
         throw new RuntimeException(e);
       }
     }
-  }
-
-
-  public Node getNode() {
-    return node;
   }
 
   public EbeanServer getServer() {
